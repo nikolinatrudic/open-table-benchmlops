104dbfc0f1e1ad6c28a5612b122786721b0e2e37
nikolinatrudic <nikolinatrudic98@gmail.com>
2024-02-15 23:27:03 +0100

    Add run.py script base for starting benchmarks and add utils script for helper functions

---
+++ benchmark/run.py
@@ -0,0 +1,34 @@
+from loguru import logger
+import click
+from pyspark.sql import SparkSession
+
+from utils import get_container_ip
+
+def create_spark_session(app_name: str, spark_cluster_ip: str):
+    spark = SparkSession.builder.master(f"spark://{spark_cluster_ip}:7077").appName(app_name).getOrCreate()
+    return spark
+
+def run_time_travel_benchmark(spark):
+    logger.info("Running time travel benchmark")
+
+def run_ingestion_benchmark(spark):
+    logger.info("Running ingestion benchmark")
+
+@click.command()
+@click.option('--benchmark', type=click.Choice(['time-travel', 'ingestion'], case_sensitive=False), help='The benchmark to run.')
+def main(benchmark):
+    spark_cluster_ip = get_container_ip("spark-master")
+    spark = create_spark_session("BenchmarkRunner", spark_cluster_ip)
+
+    match benchmark:
+        case "time-travel":
+            run_time_travel_benchmark(spark)
+        case "ingestion":
+            run_ingestion_benchmark(spark)
+        case _:
+            logger.warning("Please specify a valid benchmark to run.")
+    
+    spark.stop()
+
+if __name__ == '__main__':
+    main()
---
+++ benchmark/utils.py
@@ -0,0 +1,22 @@
+import docker
+from loguru import logger
+
+from typing import Optional
+
+def get_container_ip(container_name: str) -> Optional[str]:
+    client = docker.from_env()
+    try:
+        container = client.containers.get(container_name)
+        networks = container.attrs['NetworkSettings']['Networks']
+        
+        ip_address = None
+        for network_name, network_details in networks.items():
+            ip_address = network_details['IPAddress']
+
+        return ip_address
+    except docker.errors.NotFound:
+        logger.error(f"Container {container_name} not found.")
+        return None
+    except Exception as e:
+        logger.error(f"Error {e} happened.")
+        return None
